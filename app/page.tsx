094b8d24a5c37a266e96e113ebf5a2c333778cba
coilt <cameraboi@gmail.com>
2024-01-16 02:02:58 +0700

    h-full added

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,16 @@ import Image from 'next/image'
 export default function Home () {
 return (
 
-<div>
+<div className='h-full'>
 <Head>
   <title>Katerina Ernst Filmmaker Photographer</title>
    <meta name="description" content="next app" />
   <link rel='icon' href='/favicon.ico'/>
-
 </Head>
 
   
 
- <main className='text-red-100'>
+ <main className='h-full'>
 <p>Placeholder for main</p>
  </main>
 
