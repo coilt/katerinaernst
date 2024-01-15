ad7f392ab32e07e75658475b9e246c962cef4f39
coilt <cameraboi@gmail.com>
2024-01-16 03:44:59 +0700

    Updated tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,78 @@
-import Head from 'next/head'
-import Image from 'next/image'
+"use client";
+import { createContext } from "react";
+import { Tab } from "@headlessui/react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
+const tabs = [
+  {
+    key: "all",
+    display: "All",
+  },
+  {
+    key: "people",
+    display: "People",
+  },
+  {
+    key: "videos",
+    display: "Videos",
+  },
 
-export default function Home () {
-return (
+  {
+    key: "about me",
+    display: "About",
+  },
+];
 
-<div className='h-full'>
-<Head>
-  <title>Katerina Ernst Filmmaker Photographer</title>
-   <meta name="description" content="next app" />
-  <link rel='icon' href='/favicon.ico'/>
-</Head>
+export default function Home() {
+  return (
+    <div className="flex flex-col h-full bg-[url('../public/images/decoration/photographer.jpg')] bg-top bg-cover">
+      <Head>
+        <title>Katerina Ernst Filmmaker Photographer</title>
+        <meta name="description" content="next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  
+      <header className="flex justify-between items-center h-[90px] px-6">
+        <div className="contents-center">Katerina Ernst</div>
+        <Link
+          href="mailto:vocalaboca@gmail.com"
+          className="rounded-3xl bg-white text-stone-800 px-4 py-3 hover:bg-opacity-90"
+        >
+          Get in touch
+        </Link>
+      </header>
 
- <main className='h-full'>
-<p>Placeholder for main</p>
- </main>
+      <main className="grow">
+        <div className="flex flex-col items-center">
+          <Tab.Group>
+            <Tab.List className="flex items-centered gap-12">
+              {tabs.map((tab) => (
+                <Tab key={tab.key} className="p-2">
+                  {({ selected }) => (
+                    <span
+                      className={selected ? "text-white" : "text-stone-600"}
+                    >
+                      {tab.display}
+                    </span>
+                  )}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>All photos</Tab.Panel>
+              <Tab.Panel>Portraits</Tab.Panel>
+              <Tab.Panel>Videos</Tab.Panel>
+              <Tab.Panel>About Me</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </main>
 
-
-<footer>
-<p>Footer placeholder</p>
-</footer>
-</div>
-);
-
+      <footer className="h-[60px] flex justify-center items-center">
+        <p>Katerina Ernst Filmmaker | Photographer</p>
+      </footer>
+    </div>
+  );
 }
