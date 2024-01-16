94c16baa82973893c17075e0a1abcb16d0a365dd
coilt <cameraboi@gmail.com>
2024-01-17 00:50:15 +0700

    Removed apostrophes

--- app/page.tsx
+++ app/page.tsx
@@ -101,55 +101,54 @@ export default function Home() {
               <Tab.Panel className=" flex justify-center">
                 <div className="w-[600px]">
                   <p className={`my-4 ${paragraphFont.className}`}>
-                    Newton was born in Berlin, the son of Klara "Claire" (née
+                    Newton was born in Berlin, the son of Klara Claire (née
                     Marquis) and Max Neustädter, a button factory owner. His
                     family was Jewish. Newton attended the
                     Heinrich-von-Treitschke-Realgymnasium and the American
                     School in Berlin. Interested in photography from the age of
                     12 when he purchased his first camera, he worked for the
-                    German photographer Yva (Elsie Neuländer Simon) from 1936.
-                    The increasingly oppressive restrictions placed on Jews by
-                    the Nuremberg laws meant that his father lost control of the
-                    factory in which he manufactured buttons and buckles; he was
-                    briefly interned in a concentration camp on Kristallnacht, 9
-                    November 1938, which finally compelled the family to leave
-                    Germany. Newton's parents fled to Argentina. Newton was
-                    issued with a passport just after turning 18 and left
-                    Germany on 5 December 1938. At Trieste, he boarded the Conte
-                    Rosso (along with about 200 others escaping the Nazis),
-                    intending to journey to China. After arriving in Singapore,
-                    Newton found he was able to remain there, first briefly as a
-                    photographer for the Straits Times and then as a portrait
-                    photographer.
+                    German photographer Yva from 1936. The increasingly
+                    oppressive restrictions placed on Jews by the Nuremberg laws
+                    meant that his father lost control of the factory in which
+                    he manufactured buttons and buckles; he was briefly interned
+                    in a concentration camp on Kristallnacht, 9 November 1938,
+                    which finally compelled the family to leave Germany. Newtons
+                    parents fled to Argentina. Newton was issued with a passport
+                    just after turning 18 and left Germany on 5 December 1938.
+                    At Trieste, he boarded the Conte Rosso (along with about 200
+                    others escaping the Nazis), intending to journey to China.
+                    After arriving in Singapore, Newton found he was able to
+                    remain there, first briefly as a photographer for the
+                    Straits Times and then as a portrait photographer.
                   </p>
 
                   <p className={` my-4 ${paragraphFont.className}`}>
                     Newton and his wife finally settled in Paris in 1961 and the
                     work continued as a fashion photographer. His images
                     appeared in magazines including the French edition of Vogue
-                    and Harper's Bazaar. He established a particular style
-                    marked by erotic, stylised scenes, often with
-                    sado-masochistic and fetishistic subtexts. A heart attack in
-                    1970 reduced Newton's output, nevertheless his wife's
-                    encouragement led to his profile continuing to expand,
-                    especially with a big success, the 1980 studio-bound stark
-                    infinity of the "Big Nudes" series. His "Naked and Dressed"
-                    portfolio followed and in 1992 "Domestic Nudes" which marked
-                    the pinnacle of his erotic-urban style, these series all
-                    underpinned with the prowess of his technical skills. Newton
-                    also worked in portraiture and more fantastical studies.
-                    Newton shot a number of pictorials for Playboy, including
-                    pictorials of Nastassja Kinski and Kristine DeBell. Original
-                    prints of the photographs from his August 1976 pictorial of
-                    DeBell, "200 Motels, or How I Spent My Summer Vacation" were
-                    sold at auctions of Playboy archives by Bonhams in 2002 for
-                    $21,075, and by Christie's in December 2003 for $26,290.
-                    "Three Boys from Pasadena" In 2009, June Browne
-                    conceptualised a tribute exhibition to Newton, based on
-                    three photographers that befriended Newton in Los Angeles in
-                    1980: Mark Arbeit, Just Loomis, and George Holz. All three
-                    had been photography students at The Art Center College of
-                    Design in Pasadena, California.
+                    and Harpers Bazaar. He established a particular style marked
+                    by erotic, stylised scenes, often with sado-masochistic and
+                    fetishistic subtexts. A heart attack in 1970 reduced Newtons
+                    output, nevertheless his wifes encouragement led to his
+                    profile continuing to expand, especially with a big success,
+                    the 1980 studio-bound stark infinity of the "Big Nudes"
+                    series. His "Naked and Dressed" portfolio followed and in
+                    1992 "Domestic Nudes" which marked the pinnacle of his
+                    erotic-urban style, these series all underpinned with the
+                    prowess of his technical skills. Newton also worked in
+                    portraiture and more fantastical studies. Newton shot a
+                    number of pictorials for Playboy, including pictorials of
+                    Nastassja Kinski and Kristine DeBell. Original prints of the
+                    photographs from his August 1976 pictorial of DeBell, "200
+                    Motels, or How I Spent My Summer Vacation" were sold at
+                    auctions of Playboy archives by Bonhams in 2002 for $21,075,
+                    and by Christies in December 2003 for $26,290. "Three Boys
+                    from Pasadena" In 2009, June Browne conceptualised a tribute
+                    exhibition to Newton, based on three photographers that
+                    befriended Newton in Los Angeles in 1980: Mark Arbeit, Just
+                    Loomis, and George Holz. All three had been photography
+                    students at The Art Center College of Design in Pasadena,
+                    California.
                   </p>
                   <p className={` my-4 ${paragraphFont.className}`}>
                     All three became friends with Helmut and June Newton and to
