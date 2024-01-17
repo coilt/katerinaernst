194bd2d6e638a0651549e17417e5d85f3426b485
coilt <cameraboi@gmail.com>
2024-01-17 21:03:45 +0700

    Updated getImage disabled type restrictions

--- app/utils/getImages.js
+++ app/utils/getImages.js
@@ -1,19 +1,15 @@
 // utils/getImages.js
 const getImages = () => {
   try {
-    // Use require.context to dynamically import images from the 'public/images' folder
     const imageContext = require.context(
       "/public/images",
       false,
       /\.(png|jpe?g|gif|svg)$/
     );
-
-    // Get the list of image file paths
     const imageFiles = imageContext.keys();
 
-    // Create an array of image objects with the src property
     const images = imageFiles.map((file) => ({
-      src: `/images/${file.replace("./", "")}`, // Adjust as needed
+      src: `/images/${file.replace("./", "")}`,
     }));
 
     return images;
