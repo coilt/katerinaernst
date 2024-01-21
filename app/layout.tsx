2ef66ab8d391382683a31273286e653dfdf06517
coilt <cameraboi@gmail.com>
2024-01-21 21:32:51 +0700

    Fixed errors in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  openGraph: {
-    title: "Katerina Ernst Filmmaker | Photographer",
-    description: "Filmmaker | Photographer portfolio",
-  },
-};
-
+import { Antonio, Inter } from "next/font/google";
 import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const antonio = Antonio({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -19,3 +15,11 @@ export default function RootLayout({
     </html>
   );
 }
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Katerina Ernst Title",
+    description: "Confide your inner thoughts and get wise advice from legends",
+  },
+};
