31d4f2cf9f1f985c47b42031c1af68112f824297
coilt <cameraboi@gmail.com>
2024-01-21 23:33:34 +0700

    Updated favicons

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,13 +1,19 @@
-import { AppProps } from "next/app"
-import { Antonio } from 'next/font/google'
- 
+import { AppProps } from "next/app";
+import { Antonio } from "next/font/google";
+
 const antonio = Antonio({
-  weight: '100',
-  subsets: ['latin'],
+  weight: "100",
+  subsets: ["latin"],
   variable: "--font-antonio",
 });
- 
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className={antonio.className}/>;
-        
+  return <Component {...pageProps} className={antonio.className} />;
 }
+
+<Head>
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+</Head>;
