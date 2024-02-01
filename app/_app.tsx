00e03f27f00f7288ac1a244eedf107d734bb2705
coilt <cameraboi@gmail.com>
2024-02-02 01:18:13 +0700

    Forbid scale on mobile

--- app/_app.tsx
+++ app/_app.tsx
@@ -9,7 +9,10 @@ const antonio = Antonio({
 });
 
 <Head>
-  <meta name="viewport" content="width=device-width, user-scalable=no" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+  />
 </Head>;
 
 export default function App({ Component, pageProps }: AppProps) {
